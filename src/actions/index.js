1b1c0fac39b828c43c8b0eaffb02e36c79105aea
Jan Václavík <me@janvaclavik.com>
2018-01-29 12:30:51 +0100

    upgrade SearchBar to container

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const WEATHER_API_KEY = '60863eacd085c362630496564f490213';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API_KEY}`;
 
@@ -5,7 +7,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city}`;
+    const request = axios.get(url);
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
