24b40a29e1d02e229af1c39661767a9adad5b647
Jan Václavík <me@janvaclavik.com>
2018-01-29 11:11:15 +0100

    connect redux to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
+import reducers from './reducers';
 import registerServiceWorker from './tools/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>,
+
+    document.getElementById('root')
+);
 registerServiceWorker();
