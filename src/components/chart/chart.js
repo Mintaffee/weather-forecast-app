91df54d9263cf061dd6eb6943c5230aa521b0f6c
Jan Václavík <me@janvaclavik.com>
2018-01-30 14:44:20 +0100

    add average in chart

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import {
     Sparklines,
     SparklinesLine,
@@ -7,6 +8,11 @@ import {
 
 import './chart.css';
 
+function average(data) {
+
+    return _.round(_.sum(data) / data.length);
+}
+
 export default props => {
     return (
         <div className="chart">
@@ -14,6 +20,7 @@ export default props => {
                 <SparklinesLine color={props.color || 'red'} />
                 <SparklinesReferenceLine type="avg" />
             </Sparklines>
+            <div>{average(props.data)}</div>
         </div>
     );
 };
